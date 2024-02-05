313948b810f68b1eb87cb50150c9d62d2460a967
jackjitendra768 <jackjitendra768@gmail.com>
2024-02-05 19:43:13 +0530

    update

---
+++ 100_interview/script.js
@@ -0,0 +1,30 @@
+// 1.
+
+// let a = [];
+// let b = [];
+// console.log(a == b);
+// console.log(a === b);
+
+// 2.
+
+// let c = [];
+// let d = c;
+// console.log(c == d);
+// console.log(c === d);
+
+// 3.
+
+// let a=[20];
+// let b=[20];
+// console.log(a[0]==b[0]);
+// console.log(a[0]===b[0]);
+
+// 4.
+
+// let z = [1,2,3,4];
+// let a={name:"amit"};
+// console.log(...z);
+
+// 5.
+
+console.log(typeof NaN);
